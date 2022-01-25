f9741b72ffdcbf48a4294b5ce573f3806e8c0864
MOSQUEDACORDOVA <elmosquedacordova@gmail.com>
2022-01-25 10:21:24 -0600

    ajustes visuales prueba

--- src/public/assets/js/PYT672/grupos.js
+++ src/public/assets/js/PYT672/grupos.js
@@ -475,7 +475,7 @@ function cargarTablaIntensivos() {
                             </div>
 
                             <!---- PROF ---->
-                            <div class="me-1">
+                            <div class="">
                                 <span class="item-user me-75"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-user font-small-4"><path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"></path><circle cx="12" cy="7" r="4"></circle></svg></span>
                                 <small class="emp_post text-muted">Prof.</small><br><small class="emp_post">Mosqueda Cor.</small>
                             </div>
